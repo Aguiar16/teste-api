e4257f6b5fe006e36b218acbac285db10f8c9544
aguiar16 <lucas16k9@gmail.com>
2020-01-07 18:40:25 -0300

    concertar o zip

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,54 @@
+class BoardsController < ApplicationController
+  before_action :set_board, only: [:show, :update, :destroy]
+
+  # GET /boards
+  def index
+    @boards = Board.all
+
+    render json: @boards
+  end
+
+  # GET /boards/1
+  def show
+    render json: @board
+  end
+
+  # POST /boards
+  def create
+    @board = Board.new(board_params)
+
+    if @board.save
+      render json: @board, status: :created, location: @board
+      @list = List.create(name: "To Do", board_id: @board.id)
+      @list = List.create(name: "Doing", board_id: @board.id)
+      @list = List.create(name: "Done", board_id: @board.id)
+    else
+      render json: @board.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /boards/1
+  def update
+    if @board.update(board_params)
+      render json: @board
+    else
+      render json: @board.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /boards/1
+  def destroy
+    @board.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_board
+      @board = Board.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def board_params
+      params.require(:board).permit(:name, :listName, :list)
+    end
+end
